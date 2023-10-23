17da389be88dcc8fa157135e419e1cc75c39ec69
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-10-23 09:28:44 +0900

    update

--- regist/input.php
+++ regist/input.php
@@ -3,9 +3,31 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>My Tweet</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
-    <h2>Regist</h2>
+    <main id="id" class="d-flex justify-content-center">
+        <div class="w-50 mt-3 p-5 bg-light">
+            <h2 class="h2 mb-3 fw-normal">Regist</h2>
+            <form action="" method="post">
+                <div class="form-floating mb-2">
+                    <input type="text" name="name" class="form-control">
+                    <label for="" class="form-label">Name</label>
+                </div>
+                <div class="form-floating mb-2">
+                    <input type="email" name="email" class="form-control">
+                    <label for="" class="form-label">Email</label>
+                </div>
+                <div class="form-floating mb-2">
+                    <input type="password" name="password" class="form-control">
+                    <label for="" class="form-label">Password</label>
+                </div>
+                <div>
+                    <button class="w-100 btn btn-primary">Regist</button>
+                </div>
+            </form>
+        </div>
+    </main>
 </body>
 </html>
