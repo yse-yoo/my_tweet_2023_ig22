198bbf02f44d155701783b66c273bd3efb828327
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-10-30 13:35:15 +0900

    add components

--- index.php
+++ index.php
@@ -26,21 +26,9 @@ $tweets = $tweet->get();
     <div class="container-fluid">
         <div class="row">
             <header class="col-md-3">
-                <nav id="side-menu">
-                    <ul>
-                        <li>
-                            <img src="svg/home.svg">
-                            <a href="">Home</a>
-                        </li>
-                        <li>
-                            <img src="svg/profile.svg">
-                            <a href="">Profile</a>
-                        </li>
-                        <li>
-                            <a href="user/logout.php">Sign out</a>
-                        </li>
-                    </ul>
-                </nav>
+
+                <?php include('app/views/components/tweet_menu.php') ?>
+
                 <div class="tweet-user">
                     <span class="fw-bold">@<?= $auth_user['name'] ?></span>
                 </div>
@@ -48,67 +36,14 @@ $tweets = $tweet->get();
 
             <main class="col-md-9">
                 <h2>Home</h2>
-
                 <div class="row">
-                    <!-- Tweet投稿フォーム -->
-                    <form action="tweet/add.php" method="post">
-                        <textarea required name="message" class="form-control" placeholder="いまどうしてる？"></textarea>
-                        <div class="mt-3 mb-3 text-center">
-                            <button class="btn btn-primary rounded-pill w-25">Tweet</button>
-                        </div>
-                    </form>
+                    <?php include('app/views/components/tweet_form.php') ?>
                 </div>
 
                 <div class="row">
                     <!-- Tweetの繰り返し表示 -->
                     <?php foreach ($tweets as $value) : ?>
-                        <div class="tweet d-flex">
-                            <!-- profile image -->
-                            <div class="profile-image">
-                                <img src="images/me.png">
-                            </div>
-                            <!-- tweet body -->
-                            <div class="tweet-body">
-                                <!-- user info -->
-                                <div class="tweet-user">
-                                    <span class="fw-bold">@<?= $value['user_name'] ?></span>
-                                    <span class="ms-1 text-secondary"><?= date('Y/m/d H:i', strtotime($value['created_at'])) ?></span>
-                                </div>
-
-                                <!-- post -->
-                                <div class="tweet-text mt-2 mb-2">
-                                    <?= nl2br($value['message']) ?>
-                                </div>
-
-                                <!-- tweet nav -->
-                                <nav class="tweet-nav">
-                                    <ul class="d-flex">
-                                        <li>
-                                            <a href="#">
-                                                <img src="svg/bubble.svg" alt="">
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href="#">
-                                                <img src="svg/heart.svg" alt="">
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href="#">
-                                                <img src="svg/loop.svg" alt="">
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <?php if ($auth_user['id'] == $value['user_id']): ?>
-                                            <a href="tweet/delete.php?id=<?= $value['id'] ?>">
-                                                <img src="svg/trash.svg" alt="">
-                                            </a>
-                                            <?php endif ?>
-                                        </li>
-                                    </ul>
-                                </nav>
-                            </div>
-                        </div>
+                    <?php include('app/views/components/tweet.php') ?>
                     <?php endforeach ?>
                 </div>
 
